545198e3f538959973e4c0a78ce873bc28fc209c
ttanimarefin <ttanimarefin@gmail.com>
2021-10-26 22:02:22 +0600

    templating html

--- shopper/settings.py
+++ shopper/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_URL = '/static/'
+MEDIA_URL = "/image/download/"
+MEDIA_ROOT = BASE_DIR
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models.product import Product
 
 # Create your views here.
+def index(request):
+    prds=Product.get_all_products()
+    return render(request,'orders/order.html')
+    #return render(request,'index.html',{'product':prds})
