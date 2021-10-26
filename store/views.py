c372f4ad1b74b9b1f222a1414a03aa3ca50dac3a
ttanimarefin <ttanimarefin@gmail.com>
2021-10-27 01:57:26 +0600

    use Refactoring Code

--- store/views.py
+++ store/views.py
@@ -1,5 +1,4 @@
 from django.core.checks.messages import ERROR
-from django.core.validators import _ErrorMessage
 from django.db.models.expressions import Value
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
@@ -26,63 +25,64 @@ def index(request):
     return render(request, 'index.html', data)
 
 
+def validateCustomer(customer):
+    _ErrorMessage = None
+    if(not customer.first_name):
+        _ErrorMessage = "First Name Required !"
+    elif len(customer.first_name):
+        _ErrorMessage = 'First Name must be 4 char long'
+    elif customer.isExists():
+        _ErrorMessage = 'Email address Already registered'
+
+    return _ErrorMessage
+
+
+def registerUser(request):
+
+    postData = request.POST
+    first_name = postData.get('firstname')
+    last_name = postData.get('lastname')
+    phone = postData.get('phone')
+    email = postData.get('email')
+    password = postData.get('password')
+
+    value = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'phone': phone,
+        'email': email
+
+    }
+    _ErrorMessage = None
+
+    customer = Customer(first_name=first_name,
+                        last_name=last_name,
+                        phone=phone,
+                        email=email,
+                        password=password)
+
+    error_message = validateCustomer(customer)
+
+    # save
+    if not _ErrorMessage:
+        print(first_name, last_name, phone, email, password)
+
+        customer.register()
+
+        return redirect('homepage')
+    else:
+        data = {
+            'error': _ErrorMessage,
+            'values': value
+        }
+    filter_horizontal = ()
+    return render(request, 'signup.html', data)
+
+
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
+
     else:
-        postData = request.POST
-        first_name = postData.get('firstname')
-        last_name = postData.get('lastname')
-        phone = postData.get('phone')
-        email = postData.get('email')
-        password = postData.get('password')
-
+        return registerUser(request)
         # vali
-
-        value = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'phone': phone,
-            'email': email
-
-        }
-        _ErrorMessage = None
-
-        customer = Customer(first_name=first_name,
-                                last_name=last_name,
-                                phone=phone,
-                                email=email,
-                                password=password )
-
-
-        if(not first_name):
-            _ErrorMessage = "First Name Required !"
-        elif first_name:
-            if len(first_name) < 4:
-                _ErrorMessage = 'First Name must be 4 char long'
-        elif customer.isExists():
-            
-        
-       
-        
-            _ErrorMessage='Email address Already registered'
-        
-        
-        # save
-        if not _ErrorMessage:
-            print(first_name, last_name, phone, email, password)
-            
-
-
-
-                                
-            customer.register()
-
-            return redirect('homepage')
-        else:
-            data= {
-                'error': _ErrorMessage ,
-                'values': value  
-            }
-
-        return render(request, 'signup.html', data)
